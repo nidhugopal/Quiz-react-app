fc60dec8bd5383589d5f05e2bb59639633b6f8b2
Nidhu Gopal <nidhu.gopal.p@gmail.com>
2020-12-10 19:00:39 -0800

    score and play again added

--- src/components/QuizHome.js
+++ src/components/QuizHome.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import '../assets/style.css'
 import quizService from '../quizService/index'
 import QuestionBox from './QuestionBox'
+import Result from './Result'
+
 
 class QuizHome extends Component {
     state={
-        questionBank: []
+        questionBank: [],
+        score: 0,
+        responses: 0
     }
 
     getQuestions(){
@@ -16,6 +20,23 @@ class QuizHome extends Component {
         });
     }
 
+    computeAnswer = (answer, correctAnswer) =>{
+        if(answer === correctAnswer){
+            this.setState({ 
+                score: this.state.score + 1 });
+        }
+        this.setState({ 
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5 });
+    }
+
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0
+        })
+    }
+
     componentDidMount(){
         this.getQuestions();
     }
@@ -24,8 +45,18 @@ class QuizHome extends Component {
         return (
             <div>
                 <div className='title'>Quiz</div>
-                {this.state.questionBank.length > 0 && this.state.questionBank.map(({question, answers, correct, questionId}) => <QuestionBox question={question} options={answers} key={questionId} />
+                {this.state.questionBank.length > 0 && 
+                    this.state.responses < 5 &&
+                    this.state.questionBank.map(({question, answers, correct, questionId}) => 
+                    <QuestionBox 
+                    question={question} 
+                    options={answers} 
+                    key={questionId} 
+                    selected={answer => this.computeAnswer(answer, correct)}
+                    />
                 )}
+
+                {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain}/>) : null}
             </div>
         )
     }
